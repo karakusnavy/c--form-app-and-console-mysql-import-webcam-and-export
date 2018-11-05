b5de94d2efb1084aa5e5997e0805fc30659bd0b0
Samed Karakus <karakusnavy@gmail.com>
2018-11-05 18:31:41 +0300

    updated

---
+++ GetPhoto/GetPhoto/Form1.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetPhoto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MySqlConnection con = new MySqlConnection("Server=localhost;Database=example_webcam;Uid=root;Pwd=;");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlCommand cmd = new MySqlCommand("select * from pictures",con);
+            con.Open();
+            MySqlDataReader read = cmd.ExecuteReader();
+            while (read.Read())
+            pictureBox1.Image = new Bitmap(new MemoryStream(Convert.FromBase64String(read["image"].ToString())));
+            con.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome");
+        }
+    }
+}
---
+++ Windows/Windows/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge;
+using System.Drawing;
+using System.IO;
+using MySql.Data.MySqlClient;
+using System.Drawing.Imaging;
+
+namespace Windows
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mysql Photo Importer With Console Applicaon");
+            Console.Write("Start YES/NO : ");
+            string request = Console.ReadLine().ToUpper();
+            if (request == "YES") take_photo();
+            else return;            
+            void take_photo(){
+                MySqlConnection con = new MySqlConnection("Server=localhost;Database=example_webcam;Uid=root;Pwd=;");
+                FilterInfoCollection webcam;
+                VideoCaptureDevice cam;
+                webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                cam = new VideoCaptureDevice(webcam[0].MonikerString);
+                cam.Start();
+                cam.NewFrame += new NewFrameEventHandler(cam_NewFrame);
+                void cam_NewFrame(object sender, NewFrameEventArgs eventArgs){
+                    Bitmap bit = (Bitmap)eventArgs.Frame.Clone();
+                    System.IO.MemoryStream ms = new MemoryStream();
+                    bit.Save(ms, ImageFormat.Jpeg);
+                    byte[] byteImage = ms.ToArray();
+                    var SigBase64 = Convert.ToBase64String(byteImage);
+                    Console.WriteLine(SigBase64);
+                    MySqlCommand cmd = new MySqlCommand("insert into pictures(image) values('" + SigBase64 + "')", con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("--------------------------------------------------");
+                    Console.WriteLine("Succesfull! ");
+                    Console.WriteLine("--------------------------------------------------");
+                    cam.Stop();                    
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
